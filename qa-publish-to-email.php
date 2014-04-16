13c29bab848c38879709154cd864c6cb1992c3e1
Ali Tavakoli <ali.tavakoli@gmail.com>
2014-04-16 12:07:16 -0500

    refactored email body generation; added email trail

--- qa-publish-to-email.php
+++ qa-publish-to-email.php
@@ -26,6 +26,9 @@ class qa_publish_to_email_event
 
 		if ($option == 'plugin_publish2email_plaintext_only')
 			return false;
+
+		if ($option == 'plugin_publish2email_show_trail')
+			return true;
 	}
 
 	function admin_form(&$qa_content)
@@ -38,6 +41,7 @@ class qa_publish_to_email_event
 			qa_opt('plugin_publish2email_fav_categories_only', (int)qa_post_text('plugin_publish2email_fav_cats_field'));
 			qa_opt('plugin_publish2email_use_bcc', (int)qa_post_text('plugin_publish2email_use_bcc_field'));
 			qa_opt('plugin_publish2email_plaintext_only', (int)qa_post_text('plugin_publish2email_plaintext_only_field'));
+			qa_opt('plugin_publish2email_show_trail', (int)qa_post_text('plugin_publish2email_show_trail_field'));
 			$saved = true;
 		}
 
@@ -69,6 +73,12 @@ class qa_publish_to_email_event
 					'type' => 'checkbox',
 					'value' => qa_opt('plugin_publish2email_plaintext_only'),
 					'tags' => 'NAME="plugin_publish2email_plaintext_only_field"',
+				),
+				array(
+					'label' => 'Include dependent posts in email body (e.g. include questions when sending emails for answers)',
+					'type' => 'checkbox',
+					'value' => qa_opt('plugin_publish2email_show_trail'),
+					'tags' => 'NAME="plugin_publish2email_show_trail_field"',
 				),
 			),
 
@@ -151,17 +161,32 @@ class qa_publish_to_email_event
 
 			$mailer->Subject=$subject;
 
-			if ($params['format'] === 'html' && !qa_opt('plugin_publish2email_plaintext_only'))
+
+			// If any of the posts that need to be put in the body are HTML, make everything HTML
+			$isanyposthtml=($params['format'] === 'html');
+			if (qa_opt('plugin_publish2email_show_trail'))
 			{
-				$mailer->IsHTML(true);
-				$mailer->Body=$params['content'].'<hr><p><strong>Reply at <a href="'.$url.'">this link</a>.</strong></p>';
-				$mailer->AltBody=$params['text']."\n\n===\n\nReply at this link:\n".$url;
+				switch ($event)
+				{
+				case 'c_post':
+					// For comments, check both the parent and the question
+					// (which might be the same post, but it doesn't change the result)
+					$isanyposthtml=$isanyposthtml || ($params['question']['format'] === 'html');
+					// fall through
+				case 'a_post':
+					// For answers, just check the parent, which is the question
+					$isanyposthtml=$isanyposthtml || ($params['parent']['format'] === 'html');
+					break;
+				}
 			}
-			else
-			{
-				$mailer->IsHTML(false);
-				$mailer->Body=$params['text']."\n\n===\n\nReply at this link:\n".$url;
-			}
+
+			$ishtml=($isanyposthtml && !qa_opt('plugin_publish2email_plaintext_only'));
+
+			// Add the body and add a plaintext AltBody for HTML emails
+			$mailer->IsHTML($ishtml);
+			$mailer->Body=$this->qa_build_body($event, $url, $params, $ishtml);
+			if ($ishtml)
+				$mailer->AltBody=$this->qa_build_body($event, $url, $params, false);
 
 			if (qa_opt('smtp_active'))
 			{
@@ -211,6 +236,76 @@ class qa_publish_to_email_event
 			'arguments' => array($emails, $categoryid, QA_ENTITY_CATEGORY),
 		));
 	}
+
+	function qa_format_post($params, $ishtml)
+	{
+		if (isset($params['text']))
+			$text = $params['text'];
+		else
+			$text = qa_post_content_to_text($params['content'], $params['format']);
+
+		if ($ishtml)
+		{
+			if ($params['format'] === 'html')
+				return $params['content'];
+			else
+				return '<pre>'.$text.'</pre>';
+		}
+		else
+		{
+			return $text;
+		}
+	}
+
+	function qa_format_header($preamble, $title, $ishtml)
+	{
+		if ($ishtml)
+			return '<hr><h2>'.$preamble.'</h2><h3>'.qa_html($title).'</h3>';
+		else
+			return "\n\n===\n\n".$preamble."\n\n".$title."\n\n";
+	}
+
+	function qa_format_footer($preamble, $title, $url, $ishtml)
+	{
+		if ($ishtml)
+			return '<hr><p><strong>'.$preamble.'<a href="'.$url.'">'.$title.'</a>.</strong></p>';
+		else
+			return "\n\n===\n\n".$preamble."\n".$url."\n";
+	}
+
+	function qa_build_body($event, $url, $params, $ishtml)
+	{
+		$body=$this->qa_format_post($params, $ishtml);
+
+		if (qa_opt('plugin_publish2email_show_trail'))
+		{
+			switch ($event)
+			{
+			case 'a_post':
+				$body.=$this->qa_format_header('The above was an answer to this question:', $params['parent']['title'], $ishtml);
+				$body.=$this->qa_format_post($params['parent'], $ishtml);
+				break;
+			case 'c_post':
+				if ($params['parent']['type'] == 'Q')
+				{
+					$body.=$this->qa_format_header('The above was a comment on this question:', $params['parent']['title'], $ishtml);
+					$body.=$this->qa_format_post($params['parent'], $ishtml);
+				}
+				else
+				{
+					$body.=$this->qa_format_header('The above was a comment on this answer:', '', $ishtml);
+					$body.=$this->qa_format_post($params['parent'], $ishtml);
+
+					$body.=$this->qa_format_header('Original question:', $params['question']['title'], $ishtml);
+					$body.=$this->qa_format_post($params['question'], $ishtml);
+				}
+				break;
+			}
+		}
+
+		return $body.$this->qa_format_footer("View the entire conversation or reply at ", "this link", $url, $ishtml);
+	}
+
 };
 
 /*
